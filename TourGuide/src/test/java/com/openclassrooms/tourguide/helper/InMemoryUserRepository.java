518c79ae5dbdc4de9b1a03a23952977cc610a986
Isilud <jolan.houot@gmail.com>
2024-12-18 15:51:45 +0100

    comment unused code

--- TourGuide/src/test/java/com/openclassrooms/tourguide/helper/InMemoryUserRepository.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/helper/InMemoryUserRepository.java
@@ -1,12 +1,8 @@
 package com.openclassrooms.tourguide.helper;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -17,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import com.openclassrooms.tourguide.model.User;
 import com.openclassrooms.tourguide.repository.UserRepository;
 
-import gpsUtil.location.Location;
-import gpsUtil.location.VisitedLocation;
 import jakarta.annotation.PostConstruct;
 
 public class InMemoryUserRepository implements UserRepository {
@@ -72,28 +66,30 @@ public class InMemoryUserRepository implements UserRepository {
 		logger.debug("Created " + InternalTestHelper.getInternalUserNumber() + " internal test users.");
 	}
 
-	private void generateUserLocationHistory(User user) {
-		IntStream.range(0, 3).forEach(i -> {
-			user.addToVisitedLocations(new VisitedLocation(user.getUserId(),
-					new Location(generateRandomLatitude(), generateRandomLongitude()), getRandomTime()));
-		});
-	}
+	// private void generateUserLocationHistory(User user) {
+	// IntStream.range(0, 3).forEach(i -> {
+	// user.addToVisitedLocations(new VisitedLocation(user.getUserId(),
+	// new Location(generateRandomLatitude(), generateRandomLongitude()),
+	// getRandomTime()));
+	// });
+	// }
 
-	private double generateRandomLongitude() {
-		double leftLimit = -180;
-		double rightLimit = 180;
-		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
-	}
+	// private double generateRandomLongitude() {
+	// double leftLimit = -180;
+	// double rightLimit = 180;
+	// return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+	// }
 
-	private double generateRandomLatitude() {
-		double leftLimit = -85.05112878;
-		double rightLimit = 85.05112878;
-		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
-	}
+	// private double generateRandomLatitude() {
+	// double leftLimit = -85.05112878;
+	// double rightLimit = 85.05112878;
+	// return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+	// }
 
-	private Date getRandomTime() {
-		LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
-		return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
-	}
+	// private Date getRandomTime() {
+	// LocalDateTime localDateTime = LocalDateTime.now().minusDays(new
+	// Random().nextInt(30));
+	// return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
+	// }
 
 }
